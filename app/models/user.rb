fabbe77a6a22e181e3928a33fd44d8fbd358c772
Ellis Whaley <eewhaley@icloud.com>
2024-11-18 15:14:43 +0000

    completed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :dogs, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :encrypted_password, length: { minimum: 5, too_short: "minimum is 5 characters" }
+  validates :encrypted_password, length: { maximum: 50, too_long: "maximum is 50 characters" }
 end
