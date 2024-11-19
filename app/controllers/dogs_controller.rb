d48cda8b95775e35f205f9d01d8b85d2ff12c71f
Fergus Buscall <buscallf@gmail.com>
2024-11-19 12:28:44 +0000

    Creating dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,5 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:create]
 
   def index
     @dogs = Dog.all
@@ -7,4 +8,25 @@ class DogsController < ApplicationController
   def show
     @dog = Dog.find(params[:id])
   end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = @user
+    if @dog.save
+      redirect_to dog_path(@restaurant)
+    else
+      render "dogs/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:dog_id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:breed, :age, :price, :size, :description, :name)
+  end
+
 end
