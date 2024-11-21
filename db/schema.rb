10ecba236ab60b5cfd64b006ac76074aad91a85a
Eugenia Leong <eugenialeong@gmail.com>
2024-11-21 14:47:50 +0000

    map address update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_21_125406) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_21_144557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,7 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_21_125406) do
     t.string "image_url"
     t.float "latitude"
     t.float "longitude"
-    t.string "Address"
+    t.string "address"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
@@ -80,8 +80,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_21_125406) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
