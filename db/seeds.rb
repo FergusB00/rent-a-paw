c8b170f92a772b29260e79f0de4186091c2abbe5
Fergus Buscall <buscallf@gmail.com>
2024-11-21 13:09:32 +0000

    Individualised photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "open-uri"
+
 puts "Cleaning database..."
 Booking.destroy_all
 Dog.destroy_all
@@ -48,11 +51,27 @@ puts "Created #{User.count} users."
 
 puts "Creating dogs..."
 
-20.times do
+dog_types = [
+  { type: "Labrador", image_name: "https://cdn.britannica.com/82/232782-050-8062ACFA/Black-labrador-retriever-dog.jpg" },
+  { type: "Cocker Spaniel", image_name: "https://cdn.britannica.com/35/38935-050-E4DE59E9/Cocker-spaniel.jpg" },
+  { type: "Dachshund", image_name: "https://cdn.britannica.com/81/12481-050-6FF2A457/dachshund-coat-hair.jpg" },
+  { type: "German Shepherd", image_name: "https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg" },
+  { type: "Jack Russell", image_name: "https://cdn.britannica.com/32/8132-050-FD658B0B/terrier-Parson-Jack-Russell.jpg" },
+  { type: "Chihuahua", image_name: "https://cdn.britannica.com/44/233244-050-A65D4571/Chihuahua-dog.jpg" },
+  { type: "Collie", image_name: "https://cdn.britannica.com/25/234625-050-6070814C/Border-Collie-dog.jpg" },
+  { type: "Greyhound", image_name: "https://apupabove.com/cdn/shop/articles/Italian_Greyhound_Basic_Care_Guide_1200x1200.webp?v=1701708859" },
+  { type: "Husky", image_name: "https://cdn.britannica.com/84/232784-050-1769B477/Siberian-Husky-dog.jpg" },
+  { type: "Schnauzer", image_name: "https://cdn.britannica.com/46/79546-050-1B8F54F4/Miniature-schnauzer.jpg" },
+  { type: "Dalmatian", image_name: "https://cdn.britannica.com/47/236047-050-F06BFC5E/Dalmatian-dog.jpg" },
+  { type: "Labrador", image_name: "https://cdn.britannica.com/68/28268-004-53E03392.jpg" },
+  { type: "Labrador", image_name: "https://media.graphassets.com/resize=height:360,width:1280/output=format:webp/AX2WeBpHQImdIope0Vlk?width=1280" }
+]
+
+dog_types.each do |dog|
   Dog.create(
     name: Faker::Creature::Dog.name,
-    breed: Faker::Creature::Dog.breed,
-    image_url: "landing/dog_4.jpg",
+    breed: dog[:type],
+    image_url: dog[:image_name],
     age: rand(1..12),
     price: rand(20..100),
     size: "Large",
@@ -60,6 +79,21 @@ puts "Creating dogs..."
     user_id: users.sample.id
   )
 end
+
+# 16.times do
+#   Dog.create(
+#     name: Faker::Creature::Dog.name,
+#     breed: Faker::Creature::Dog.breed,
+#     image_url: "landing/dog_4.jpg",
+#     age: rand(1..12),
+#     price: rand(20..100),
+#     size: "Large",
+#     description: Faker::Creature::Dog.meme_phrase,
+#     user_id: users.sample.id
+#   )
+# end
+
+
 
 dogs = Dog.all
 
